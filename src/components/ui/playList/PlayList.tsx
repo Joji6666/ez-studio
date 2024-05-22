46a74688294012484d96bea03484c43385073046
KimJin <joji6666@github.com>
2024-05-22 15:44:30 +0900

    fix playlist

--- src/components/ui/playList/PlayList.tsx
+++ src/components/ui/playList/PlayList.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import "./style/play_list.scss";
 import usePlayList from "./store/usePlayList";
+import useTopToolbar from "../topToolbar.tsx/store/useTopToolbar";
 
 const PlayList = () => {
-  const { playListTracks } = usePlayList();
+  const { playListTracks, isPlayListPlaying, increaseStep } = usePlayList();
+  const { bpm } = useTopToolbar();
   const [maxWidth, setMaxWidth] = useState(0);
   const [scrollX, setScrollX] = useState(0);
+  const [xPosition, setXPosition] = useState(0);
   useEffect(() => {
     // Calculate the maximum width
 
@@ -35,8 +38,34 @@ const PlayList = () => {
     }
   }, []);
 
+  useEffect(() => {
+    let interval = null;
+    if (isPlayListPlaying) {
+      const stepDuration = (60 / bpm) * 1000;
+      interval = setInterval(() => {
+        setXPosition((prevPosition) => (prevPosition + 1) % 200);
+        increaseStep();
+      }, stepDuration);
+    } else {
+      if (interval) {
+        clearInterval(interval);
+      }
+    }
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [isPlayListPlaying, bpm]);
+
   return (
     <section className="play-list-section">
+      <div
+        className="timeline-bar-line"
+        style={{
+          left: `calc(${xPosition * 7}px + 10%)`,
+        }}
+      ></div>
       <div className="play-list-top-wrapper">
         <div className="play-list-side-toolbar"></div>
         <div className="play-list-measure-bar-wrapper">
